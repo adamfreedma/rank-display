e11dfb328c38b7e9cc3058b17f2c5fb741102a48
adamfreedma <adamfreedma@gmail.com>
2022-07-19 13:08:59 +0300

    Added docstring to valorant.py.

--- valorant.py
+++ valorant.py
@@ -2,7 +2,15 @@ import http.client
 import json
 
 def get_rank(name, tag):
+    """returns a player's valorant rank
 
+    Args:
+        name (str): player's name
+        tag (str): player's tag
+
+    Returns:
+        str: player's rank
+    """
     conn = http.client.HTTPSConnection("api.henrikdev.xyz")
 
     conn.request("GET", f"/valorant/v1/mmr/eu/{name}/{tag}",)
