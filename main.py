78078b1f493e5114cf1516c4f44bc9bf6575f1d9
adamfreedma <adamfreedma@gmail.com>
2022-08-27 11:25:41 +0300

    Added text drawing for the kda.

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import rocket_league
 import valorant
 import clash_of_clans
 import time
+from PIL import Image, ImageDraw, ImageFont
 
 def main():
 
     COC_PLAYER_TAG = "#90CVPR2RP"
     EPIC_ID = "d6065524054c447780dc90335ec85bbd"
-    VALORANT_NAME = "freedaddy#daddy"
+    VALORANT_NAME = "freedaddy#Daddy"
     # all periods are in seconds
     RL_UPDATE_PERIOD = 6 * 60 * 60
     COC_UPDATE_PERIOD = 60
@@ -16,13 +17,15 @@ def main():
     curr_time = time.time()
     last_rl_update = curr_time 
     last_coc_update = curr_time 
-    last_valorant_update = curr_time 
+    last_valorant_update = 0 
 
     valorant_rank = ""
+    new_game = False
     rl_rank = ""
     coc_heroes_progress = 0
 
     coc_player = clash_of_clans.CocPlayer(COC_PLAYER_TAG)
+    valorant_player = valorant.ValorantPlayer(*VALORANT_NAME.split("#"))
 
     while True:
 
@@ -31,14 +34,21 @@ def main():
         if (curr_time - last_rl_update) > RL_UPDATE_PERIOD:
             rl_rank = rocket_league.get_rank(EPIC_ID)
         
-        if (curr_time - last_coc_update) > COC_UPDATE_PERIOD:
-            coc_player.update_player_data()
-            coc_heroes_progress = coc_player.heroes_progress()
+        # if (curr_time - last_coc_update) > COC_UPDATE_PERIOD:
+        #     coc_player.update_player_data()
+        #     coc_heroes_progress = coc_player.heroes_progress()
 
         if (curr_time - last_valorant_update) > VALORANT_UPDATE_PERIOD:
-            valorant_rank = valorant.get_rank(*VALORANT_NAME.split("#"))
+            new_game, valorant_kda = valorant_player.get_kda()
 
-        print("valorant:", valorant_rank)
+        if new_game:
+            img = Image.open('background.png')
+            draw = ImageDraw.Draw(img)
+            font = ImageFont.truetype(r'arial.ttf', 11)
+            draw.text((0, 0), str(valorant_kda), fill=(int(100 / valorant_kda), int(100 *  valorant_kda), 0), font=font)
+            img.save("kda.png")
+
+        print("valorant:", valorant_kda)
         print("rocket_league:", rl_rank)
         print("coc:", coc_heroes_progress)
 
