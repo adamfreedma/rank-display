ddc088a45fc4efec30f3c1323240dd611fe31a5b
adamfreedma <adamfreedma@gmail.com>
2022-08-31 20:35:01 +0300

    Switched to the controller from direct control.

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import valorant
 import clash_of_clans
 import time
 from PIL import Image
-from divoom import Pixoo
+from divoom_controller import DivoomController
 from PIL import Image, ImageDraw, ImageFont
+from threading import Thread
 
 def main():
 
-    pixoo = Pixoo('11:75:58:ae:8e:cc')
-    pixoo.connect()
+    PIXOO_ADDRESS = '11:75:58:ae:8e:cc'
+
+    pixoo = DivoomController(PIXOO_ADDRESS)
+
 
     COC_PLAYER_TAG = "#90CVPR2RP"
     EPIC_ID = "d6065524054c447780dc90335ec85bbd"
@@ -30,6 +33,9 @@ def main():
 
     coc_player = clash_of_clans.CocPlayer(COC_PLAYER_TAG)
     valorant_player = valorant.ValorantPlayer(*VALORANT_NAME.split("#"))
+
+    th = Thread(target=pixoo.run)
+    th.start()
 
     while True:
 
@@ -52,6 +58,7 @@ def main():
             font = ImageFont.truetype(r'arial.ttf', 11)
             draw.text((0, 0), str(valorant_kda), fill=(int(100 / valorant_kda), int(100 *  valorant_kda), 0), font=font)
             img.save("kda.png")
+            pixoo.cycle_set("kda", "kda.png")
 
         # print("valorant:", valorant_rank)
         # print("rocket_league:", rl_rank)
@@ -60,12 +67,12 @@ def main():
         if(valorant_rank):
             filename = valorant_rank.lower().replace(' ', '')
             print("drawing", valorant_rank)
-            pixoo.draw_pic("rank-images\\" + filename + ".png")
+            pixoo.cycle_set("valorant", "rank-images\\" + filename + ".png")
         
         if new_game:
-            pixoo.draw_pic("kda.png")
+            pixoo.push("kda.png", 20)
 
-        time.sleep(60)
+        time.sleep(5)
 
 if __name__ == "__main__":
     main()
