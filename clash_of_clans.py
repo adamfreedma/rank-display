1d7fffb0beadc6bfa405ecaf069b62e04a568f85
adamfreedma <adamfreedma@gmail.com>
2022-09-09 19:25:36 +0300

    Updated hero percentage to include royal chapion.

--- clash_of_clans.py
+++ clash_of_clans.py
@@ -7,14 +7,14 @@ class CocPlayer:
     def __init__(self, player_tag):
 
         self.player_tag = player_tag
-        self.api_key = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjdlMDZlZGZlLTBjMWMtNDc3My04Yzc2LTM1NDdiM2IwMTkzMyIsImlhdCI6MTY1ODIyNjAxMywic3ViIjoiZGV2ZWxvcGVyLzc2NDMyMWJhLTM4OTgtNmRlYy1iOTk0LWZjNzk3YTUwMTE0OCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjc3LjEzNy43OC4yNTQiXSwidHlwZSI6ImNsaWVudCJ9XX0.nUd8wx1HFkxzy8RJzC4QdwgvzZPfz_-OG5xZbO6M1_7GvdIvoiNemQ8LL7JjiPX3Gb5weAgPdg2tTGlftcFFIg"
+        self.api_key = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijg2NDA1MzZlLTJhY2QtNGE0MS1hYjU1LTE3ZDE3YmE3ODkwZCIsImlhdCI6MTY2MjcyMjIzMywic3ViIjoiZGV2ZWxvcGVyLzc2NDMyMWJhLTM4OTgtNmRlYy1iOTk0LWZjNzk3YTUwMTE0OCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjc3LjEzNy42Ny4zNCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.q_0Y_zMNz1Skgqmm6OQZFJZgPH5es7Jl0HvmsLyU9vojc_gchVISytNtGGGlNNBPTn4PPLAZKBOujznGCSrx3Q"
 
         self.data = {None}
 
 
     def update_player_data(self):
 
-        api_key = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjdlMDZlZGZlLTBjMWMtNDc3My04Yzc2LTM1NDdiM2IwMTkzMyIsImlhdCI6MTY1ODIyNjAxMywic3ViIjoiZGV2ZWxvcGVyLzc2NDMyMWJhLTM4OTgtNmRlYy1iOTk0LWZjNzk3YTUwMTE0OCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjc3LjEzNy43OC4yNTQiXSwidHlwZSI6ImNsaWVudCJ9XX0.nUd8wx1HFkxzy8RJzC4QdwgvzZPfz_-OG5xZbO6M1_7GvdIvoiNemQ8LL7JjiPX3Gb5weAgPdg2tTGlftcFFIg"
+        api_key = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijg2NDA1MzZlLTJhY2QtNGE0MS1hYjU1LTE3ZDE3YmE3ODkwZCIsImlhdCI6MTY2MjcyMjIzMywic3ViIjoiZGV2ZWxvcGVyLzc2NDMyMWJhLTM4OTgtNmRlYy1iOTk0LWZjNzk3YTUwMTE0OCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjc3LjEzNy42Ny4zNCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.q_0Y_zMNz1Skgqmm6OQZFJZgPH5es7Jl0HvmsLyU9vojc_gchVISytNtGGGlNNBPTn4PPLAZKBOujznGCSrx3Q"
 
         header = {
             'authorization': api_key
@@ -36,8 +36,7 @@ class CocPlayer:
 
         progress = self.hero_sum() - th_max_hero_sum[current_th - 1]
         progress_traget = th_max_hero_sum[current_th] - th_max_hero_sum[current_th - 1]
-
-        percentage = progress / progress_traget * 100
+        percentage = int(progress / progress_traget * 100)
 
         return percentage
 
@@ -46,5 +45,6 @@ class CocPlayer:
     get_king = lambda self : self.data["heroes"][0]["level"]
     get_queen = lambda self : self.data["heroes"][1]["level"]
     get_warden = lambda self : self.data["heroes"][2]["level"]
+    get_rc = lambda self: self.data["heroes"][4]["level"] # 3 is battle machine, from the builder base (unrealated to the needed calculation)
 
-    hero_sum = lambda self : self.get_king() + self.get_queen() + self.get_warden()
+    hero_sum = lambda self : self.get_king() + self.get_queen() + self.get_warden() + self.get_rc()
